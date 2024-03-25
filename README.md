3c87d5672a628716d2a7583214379e144f280ee0
gitusergb <g4ur131@gmail.com>
2024-03-25 13:58:37 +0530

    socialMediaApp_graphQL

---
+++ README.md
@@ -0,0 +1,26 @@
+# Social Media API using NodeJS
+
+## Overview
+- This project involves the development of a Social Media API using NodeJS. 
+- The focus is on demonstrating proficiency in Data Structures and Algorithms (DSA), Object-Oriented Programming (OOP) concepts, ExpressJS, and GraphQL. 
+- The API simulates basic functionalities of a social media platform, including user registration, posting content, and following other users. 
+
+### Prerequisites
+
+Make sure you have Node.js installed on your machine. You can download it from [here](https://nodejs.org/).
+
+### Installation
+
+1. Clone the repository.
+
+   git clone https://github.com/gitusergb/*****
+
+### Backend link :
+
+## Backend
+- Server is running at http://localhost:3000
+- I had to build an Express application. 
+- used  https://studio.apollographql.com/sandbox/explorer for api endpoints
+- Also used MongoDB Atlas for this.
+- one can read, update, and delete posts. 
+- user can like , subscribe , and comment on the posts.
---
+++ index.js
@@ -0,0 +1,72 @@
+const { ApolloServer} = require('apollo-server');
+const PubSub = require('PubSub');
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const {connection} = require("./db")
+
+const {MONGODB}= require('./config.js')
+const pubsub = new PubSub();
+
+const PORT = process.env.port||4000;
+
+// const typeDefs = gql`
+// type post{
+//     id:ID!
+//     body: String!
+//     username: String!
+//     createdAt: String!
+// }
+// type Query{
+//     getPosts:[post]
+// }
+// `;
+// const resolvers ={
+//   Query:{
+//         // sayHi:()=>`Hello World!!!!!!!`
+//         async getPosts(){
+//             try{
+//                 const posts=await post.find();
+//                 return posts;
+//             }catch(err){
+//                 throw new Error(err);
+//             }
+//         }
+//     }
+// };
+
+const server= new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({ req }) => ({ req, pubsub })
+  // context: ({ req }) => ({ req })
+});
+
+// mongoose.connect(MONGODB,{ useNewUrlParser: true,
+//   useUnifiedTopology: true,} ).then(()=>{
+//     console.log('Mongodb connected')
+//     return server.listen({port:PORT});
+// })
+// .then(res=>{
+//     console.log(`server running at ${res.url}`)
+// }).catch(err => {
+//     console.error(err)
+//   })
+
+async function startServer() {
+  try {
+    await connection;
+    console.log("Database connection Established");
+  } catch {
+    console.log("Database connection Failed");
+  }
+
+  server.listen(PORT,() => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log("Server Started");
+  });
+}
+
+startServer();
